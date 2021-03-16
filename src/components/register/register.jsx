c378d98fc1035337ab67666261789a936f5e960b
Vivek Alamuri <vivek.alamuri@gmail.com>
2021-03-15 20:10:40 -0400

    sign up card UI complete

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,5 +1,47 @@
-import React from "react"
+import React, { useState, useRef } from "react";
+import { Card, Form } from "react-bootstrap";
+import {
+  RegisterContainer,
+  DarkPinkCard,
+  ContentWrapper,
+  CardContent,
+  SubmitButton,
+  Header,
+  WhiteFormLabel,
+  Textbox,
+  SignInText,
+} from "./registerStyles";
 
 export const Register = (props) => {
-    return <h1>Register Page</h1>
-}
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  return (
+    <RegisterContainer>
+      <ContentWrapper classname="w-100">
+        <DarkPinkCard>
+          <CardContent>
+            <Header>Sign Up</Header>
+            <Form>
+              <Form.Group id="email">
+                <WhiteFormLabel>Email</WhiteFormLabel>
+                <Textbox type="email" required ref={emailRef}></Textbox>
+              </Form.Group>
+              <Form.Group id="password">
+                <WhiteFormLabel>Password</WhiteFormLabel>
+                <Textbox type="password" required ref={passwordRef}></Textbox>
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <WhiteFormLabel>Confirm Password</WhiteFormLabel>
+                <Textbox type="password" required ref={passwordConfirmRef}></Textbox>
+              </Form.Group>
+              <SubmitButton type="submit">Sign Up</SubmitButton>
+            </Form>
+          </CardContent>
+        </DarkPinkCard>
+        <SignInText>Already Have an account? Log In</SignInText>
+      </ContentWrapper>
+    </RegisterContainer>
+  );
+};
---
+++ src/components/register/registerStyles.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  Button,
+} from "react-bootstrap";
+
+const gray = "#ccc";
+const darkpink = "#853F3F";
+const lightpink = "#F06B6B";
+const palepink = "#f2b8b8"
+
+export const RegisterContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: ${palepink};
+  max-width: 100%;
+`;
+
+export const DarkPinkCard = styled.div`
+  background-color: ${darkpink};
+  border-radius: 10px;
+  box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.14),
+    0px 6px 9px rgba(0, 0, 0, 0.12), 6px 6px 9px rgba(0, 0, 0, 0.2);
+  padding: 5px 1rem;
+`;
+
+export const CardContent = styled.div`
+    flex: 1 1 auto;
+    min-height: 1px;
+    padding: 1.25rem 5px;
+`
+
+export const ContentWrapper = styled.div`
+  width: 300px;
+`;
+
+export const Header = styled.h2`
+  text-align: center;
+  margin-bottom: 4px;
+  color: ${gray};
+`;
+
+export const WhiteFormLabel = styled(FormLabel)`
+  color: ${gray};
+  font-size: 18px;
+`;
+export const Textbox = styled(FormControl)`
+  background-color: ${palepink};
+  &:focus{
+    border-color: ${darkpink}
+    &&& { box-shadow: 0 0 5px rgba(133, 63, 63, 0.28); }
+    background-color: ${palepink};
+    color: ${darkpink};
+  };
+  color: ${darkpink};
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const SignInText = styled.div`
+  text-align: center;
+  margin-top: 10px;
+  width: 100%;
+`;
+
+export const SubmitButton = styled(Button)`
+  background-color: ${lightpink};
+  border-color: ${lightpink};
+  &:hover {
+    background-color: ${palepink};
+    border-color: ${palepink};
+    color: #222;
+  }
+  width: 100%;
+  margin-top: 4px;
+`;

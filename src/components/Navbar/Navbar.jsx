8d411330c54509d0a3f6b1b70c47a2f4e77bb9e8
Kshitij-Kumar123 <68606208+Kshitij-Kumar123@users.noreply.github.com>
2021-03-24 19:24:02 -0400

    Update Navbar.jsx
    add navbar with firebase login/logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
 import { Container } from './navbarStyle.jsx'
 import { FaUserCircle } from 'react-icons/fa'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext.js";
+import { Alert, Button } from "react-bootstrap";
 
-export default function Navbar() {
+export default function Navbar(props) {
+    const [error, setError] = useState("");
+    // const { logout } = useAuth;
+    const history = useHistory();
+
+    const handleLogOut = async () => {
+        setError("");
+        try {
+            let response = await useAuth.logout();
+            // history.push("/signin");
+        } catch(err) {
+            console.log(err)
+        }
+        // useAuth.logout().then(function() {
+        //     // Sign-out successful.
+        //     console.log('User Logged Out!');
+        // }).catch(function(error) {
+        //     // An error happened.
+        //     console.log(error);
+        // });
+    };
     return (
         <Container>
             <h3>Vision</h3>
+            {error && <Alert variant="warning">{error}</Alert>}
+            <Button variant="link" onClick={handleLogOut}>
+                Sign Out
+            </Button>
             <FaUserCircle style={{ fontSize: '2em'}} />
         </Container>
     )

52ca49ccec932c19420b105bd59a3a44bbfa7ee0
Kshitij-Kumar123 <68606208+Kshitij-Kumar123@users.noreply.github.com>
2021-03-27 20:21:36 -0400

    initial commit for searchbar in list

--- src/components/list/listCard.jsx
+++ src/components/list/listCard.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Element, Heading } from './listStyles.jsx'
-import { FormGroup, Button, Input } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 import { Link } from "react-router-dom";
-
+import AsyncSelect from 'react-select/async'
+import { universityOptions } from "./universities.js"
 
 export default function ListCard() {
+
+    const [university, setUniversity] = useState('')
+
+    const handleChange = (option) => {
+        setUniversity(option)
+        console.log(option)
+    }
+
+    // async handling of user input as api request will be made to fetch data 
+    const handleFilter = (inputValue) => {
+        return universityOptions.filter(i =>
+            i.label.toLowerCase().includes(inputValue.toLowerCase())
+        );
+    };
+
+    const promiseOptions = inputValue =>
+        new Promise(resolve => {
+            setTimeout(() => {
+            resolve(handleFilter(inputValue));
+        }, 1000);
+    });
+
     return (
         <Card>
             <Element>
                 <Heading>University</Heading>
-                <FormGroup>
-                    <Input type="select">
-                        <option>University of Waterloo</option>
-                        <option>University of California</option>
-                        <option>University of Toronto</option>
-                        <option>University of Calgary</option>
-                        <option>University of Ottawa</option>
-                    </Input>
-                </FormGroup>
+                <AsyncSelect loadOptions={promiseOptions} onChange={handleChange}/>
             </Element>
             <Element>
                 <Heading>Location</Heading>
-                <div>Location insert after uni selected</div>
+                <div>{university?.["location"]}</div>
             </Element>
             <Element>
+                {/* average cost to be inputted by the user */}
                 <Heading>AVG Cost</Heading>
                 <Input/>
             </Element>
             <Element>
                 <Heading>AVG ACT/SAT</Heading>
-                <span>Around some estimated amount from db</span>
+                { university && <span>ACT: {university["avg_act"]} <br/> SAT: {university["avg_sat"]}</span>}
             </Element>
             <Element>
                 <Heading>Acceptance %</Heading>
-                <span>Around some estimated amount from db</span>
+                <span>{university?.["acceptance_rate"]}</span>
             </Element>
             <Link to="/">
                 <Button size="lg" block style={{margin: '10px 0px'}} color='danger'> 
